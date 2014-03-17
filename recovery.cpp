68f025dbd3b0163ef6f71c415ad89e2293379a58
Doug Zongker <dougz@google.com>
2014-03-17 12:10:02 -0700

    refactor image resource loading code in minui
    Reduce the number of copies of libpng boilerplate.  Rename
    res_create_* functions to be more clear.  Make explicit the use of the
    framebuffer pixel format for images, and handle more combinations of
    input and output (eg, loading a grayscale image for display rather
    than use as a text alpha channel).
    
    Change-Id: I3d41c800a8f4c22b2f0167967ce6ee4d6b2b8846

--- recovery.cpp
+++ recovery.cpp
@@ -1020,8 +1020,8 @@ main(int argc, char **argv) {
     ui = device->GetUI();
     gCurrentUI = ui;
 
-    ui->Init();
     ui->SetLocale(locale);
+    ui->Init();
     ui->SetBackground(RecoveryUI::NONE);
     if (show_text) ui->ShowText(true);
 
